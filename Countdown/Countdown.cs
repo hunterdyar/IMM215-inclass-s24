3c6f625ee28ca456d00452ba0aa2ecaa2c242e06
hunterdyar <bloop@hdyar.com>
2024-03-01 11:42:49 -0500

    Add files via upload

---
+++ Countdown/Countdown.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Countdown
+{
+     private float _timer;
+     private float _lastCountdownTime;
+     public void Init(float countdownTime)
+     {
+          if (countdownTime <= 0)
+          {
+               Debug.LogWarning("There is no time to countdown. This is a bad amount of time for countdown time.");
+          }
+          _lastCountdownTime = countdownTime;
+          _timer = countdownTime;
+     }
+
+     public void Reset()
+     {
+          _timer = _lastCountdownTime;
+     }
+     
+     public void Tick(float delta)
+     {
+          _timer = _timer - delta;
+     }
+
+     public bool IsDone()
+     {
+          return _timer <= 0;
+     }
+}
---
+++ Countdown/ObjectsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObjectsTests : MonoBehaviour
+{
+    private Countdown _finalCountdown;
+    
+    void Start()
+    {
+        Debug.Log("Start");
+        _finalCountdown = new Countdown();
+        _finalCountdown.Init(3);
+    }
+    private void Update()
+    {
+        _finalCountdown.Tick(Time.deltaTime);
+        
+       
+        if (_finalCountdown.IsDone())
+        {
+            _finalCountdown.Reset();
+            DoAThing();
+        }
+    }
+
+    private void DoAThing()
+    {
+        Debug.Log("dO a tHING");
+    }
+
+}

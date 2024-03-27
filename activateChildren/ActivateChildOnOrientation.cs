9e4b044a5aa76054241119dd4735908ca965de22
hunterdyar <bloop@hdyar.com>
2024-03-27 13:34:28 -0400

    not-in-class

---
+++ activateChildren/ActivateChildOnOrientation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace RotatingStuff
+{
+    public class ActivateChildOnOrientation : MonoBehaviour
+    {
+        public Vector3 activationWorldUpAxis;
+        public float activationRange;
+        private bool lastActiveState;
+        private Animator _animator;
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            lastActiveState = false;
+            SetChildrenActive(false);
+            
+        }
+
+        void Update()
+        {
+            //Get the angle between our up axis and the activation world angle.
+            bool shouldBeActive = ShouldBeActive();
+            if (shouldBeActive != lastActiveState)
+            {
+                lastActiveState = shouldBeActive;
+                SetChildrenActive(shouldBeActive);
+            }
+        }
+
+        private bool ShouldBeActive()
+        {
+            float angle = Vector3.Angle(transform.up, activationWorldUpAxis);
+            return angle <= activationRange;
+        }
+
+        private void SetChildrenActive(bool active)
+        {
+            _animator.SetBool("active",active);
+            // foreach (Transform child in transform)
+            // {
+            //     child.gameObject.SetActive(active);
+            // }
+        }
+    }
+}
